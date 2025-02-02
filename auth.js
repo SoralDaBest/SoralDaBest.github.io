297397abc72a7c1aed0de560f990a7a125498d21
SoralDaBest <usingforprodigyonly@gmail.com>
2025-02-02 15:16:33 -0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,14 +1,16 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyD973DOB5cFx0ZiIjfQI8toUHHbaT70UeQ",
-    authDomain: "soraldabest.firebaseapp.com",
-    projectId: "soraldabest",
-    storageBucket: "soraldabest.firebasestorage.app",
-    messagingSenderId: "1046107389807",
-    appId: "1:1046107389807:web:755e4724322ba618dcb29a",
-    measurementId: "G-K6ZC913ET0"
-};
+// Ensure Firebase is initialized
+if (!firebase.apps.length) {
+    firebase.initializeApp({
+        apiKey: "AIzaSyD973DOB5cFx0ZiIjfQI8toUHHbaT70UeQ",
+        authDomain: "soraldabest.firebaseapp.com",
+        projectId: "soraldabest",
+        storageBucket: "soraldabest.firebasestorage.app",
+        messagingSenderId: "1046107389807",
+        appId: "1:1046107389807:web:755e4724322ba618dcb29a",
+        measurementId: "G-K6ZC913ET0"
+    });
+}
 
-firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 
 auth.onAuthStateChanged(user => {
